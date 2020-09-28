b8538eadc1a0cf5b80f5ca01ef2bfb52a4c36d3d
zhangxy <zxy163_work@163.com>
2020-09-28 10:49:43 +0800

    https://github.com/java-zhangxuyang/young-story.git

---
+++ src/main/java/com/young/YoungStoryApplication.java
@@ -0,0 +1,13 @@
+package com.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungStoryApplication.class, args);
+	}
+
+}
